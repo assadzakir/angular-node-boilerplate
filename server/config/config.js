b75858b63fa49e1387cdae81bdffc6764e4ba2db
Assad zakir <assadzakirhussain@gmail.com>
2018-02-10 16:52:45 +0500

    add config setting

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,9 @@
 const config = {
-  someFun:() =>  `some value`,
+    FB_PAGE_TOKEN: 'EAACuh9cHXF4BABMVyUmQqWRNZB074Ua1zgpH02fiv5VxcWwKdzmDPJXa9UenNrzssFZASZBIVMZC0aBNfvqCsZBY16XNVaGhF19wF869cK2plkM7c9WPHuHEJmXaVTCJHeSJ05sjUNB5xLedoTtkMOiT34nSTwmGfKiLx422z0bCPQ8ynvDA9',
+    FB_VERIFY_TOKEN: 'my_voice_and_my_password_ud_co_bot1',
+    API_AI_CLIENT_ACCESS_TOKEN: '20f2589167734dee80c32e74fbdece90',
+    FB_APP_SECRET: '7f583bb7fae426fd17d971e8b996298d',
+    SERVER_URL: "https://chatbo8.herokuapp.com/",
 
 };
 
